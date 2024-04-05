ed63cd0be7d4b8c8cf22d361fd1d31cd3ac77d9e
Kirill <sosisks888@mail.ru>
2024-04-06 00:21:26 +0300

    refactoring

--- TestTaskEffectiveMobile/Program.cs
+++ TestTaskEffectiveMobile/Program.cs
@@ -1,6 +1,5 @@
 ﻿using CommandLine;
 using TestTaskEffectiveMobile.Commands;
-using TestTaskEffectiveMobile.Exceptions.AddrestStartCommandIsNull;
 using TestTaskEffectiveMobile.FileServices;
 
 namespace StartProgram
@@ -9,20 +8,7 @@ namespace StartProgram
     {
         static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<FileCommand>(args).WithParsed(opts => Options(opts, args));
-        }
-
-        private static void Options(FileCommand opts, string[] args)
-        {
-            if (opts.AddresStart == null && opts.InputLogFilePath != null && opts.OutputLogFile != null && opts.AddresMask == null)
-                Parser.Default.ParseArguments<FileCommand>(args).WithParsed(opts => FileWriter.WriteAll(opts));
-
-            if (opts.AddresStart != null && opts.InputLogFilePath != null && opts.OutputLogFile != null)
-                Parser.Default.ParseArguments<FileCommand>(args).WithParsed(opts => FileWriterByDate.Write(opts));
-
-            if (opts.AddresStart != null && opts.InputLogFilePath != null && opts.OutputLogFile != null && opts.AddresMask != null)
-                Parser.Default.ParseArguments<FileCommand>(args).WithParsed(opts => FileWriterByAddresMask.Write(opts));
-            else throw new AddresCommandFileException();
+            Parser.Default.ParseArguments<FileCommand>(args).WithParsed(opts => FiileOption.Options(opts, args));
         }
     }
 }
