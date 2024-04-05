8116f77e34cd36a6ff4e6f2c56d0ab5b69ffac90
Kirill <sosisks888@mail.ru>
2024-04-06 00:14:47 +0300

    add addres mask operator

--- TestTaskEffectiveMobile/Program.cs
+++ TestTaskEffectiveMobile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CommandLine;
 using TestTaskEffectiveMobile.Commands;
+using TestTaskEffectiveMobile.Exceptions.AddrestStartCommandIsNull;
 using TestTaskEffectiveMobile.FileServices;
 
 namespace StartProgram
@@ -13,10 +14,15 @@ namespace StartProgram
 
         private static void Options(FileCommand opts, string[] args)
         {
-            if (opts.AddresStart == null && opts.InputLogFilePath != null && opts.OutputLogFile != null)
+            if (opts.AddresStart == null && opts.InputLogFilePath != null && opts.OutputLogFile != null && opts.AddresMask == null)
                 Parser.Default.ParseArguments<FileCommand>(args).WithParsed(opts => FileWriter.WriteAll(opts));
+
             if (opts.AddresStart != null && opts.InputLogFilePath != null && opts.OutputLogFile != null)
                 Parser.Default.ParseArguments<FileCommand>(args).WithParsed(opts => FileWriterByDate.Write(opts));
+
+            if (opts.AddresStart != null && opts.InputLogFilePath != null && opts.OutputLogFile != null && opts.AddresMask != null)
+                Parser.Default.ParseArguments<FileCommand>(args).WithParsed(opts => FileWriterByAddresMask.Write(opts));
+            else throw new AddresCommandFileException();
         }
     }
 }
